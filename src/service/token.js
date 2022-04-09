a9e72f32e186460ef670b59b746a2aeda1a8326f
NikitaSurani <nikitasurani16@gmail.com>
2022-04-09 11:47:42 +0530

    push changes

--- src/service/token.js
+++ src/service/token.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 exports.generateToken =  generateToken = (payload) => {
+    console.log('object payload', payload);
     return jwt.sign(payload, process.env.JSON_SECRET_KEY)
 }
 
