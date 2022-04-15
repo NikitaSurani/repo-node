32dc4008afc7ce1bc15adc160a95188c4b6bdda3
NikitaSurani <nikitasurani16@gmail.com>
2022-04-15 09:47:11 +0530

    favicon

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,2 +1,10 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/adlogin").then(()=>{console.log("Connection is established")});
+const dotenv = require('dotenv')
+// mongoose.connect("mongodb://localhost:27017/adlogin").then(()=>{console.log("Connection is established")});
+
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://knzdb:knzdb@cluster0.pcghd.mongodb.net/knzdb?retryWrites=true&w=majority")    
+    .then(() => console.log(" Mongoose is connected"))
+  
